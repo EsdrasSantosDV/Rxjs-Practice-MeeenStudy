88310f41f39a678d7e93914bb27eba0149598c74
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-03-10 07:47:17 -0300

    feat:unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,7 @@ const someObservable$ = new Observable<string>(subscriber => {
   setTimeout(()=>{ subscriber.next('Charlie');},10000);
 });
 
+
 const subscription=someObservable$.subscribe(value => console.log("sub1",value));
 setTimeout(()=>{const subscription2=someObservable$.subscribe(value=>console.log("sub2",value))},2000);
+setTimeout(()=>{subscription.unsubscribe(),console.log("cancelando a assinatura  do  primeiro")},8000)
