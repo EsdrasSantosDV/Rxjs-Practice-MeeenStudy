d27793ffcaf31c12228442121a7180947506301a
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-03-12 19:31:20 -0300

    feat:observable not logic

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 
-const someObservable$ = new Observable<string>(subscriber => {
-  setTimeout(()=> { subscriber.next('Alice')},7000);
-  setTimeout(()=>{ subscriber.next('Ben');},5000);
-  setTimeout(()=>{ subscriber.next('Charlie');},10000);
+const newObservable$ = new Observable(() => {
+  console.log("observable sendo executado");
 });
+console.log('antes da assinatura');
 
+//DESSE MODO NAÕ E ASSICORNO PQ JA E DE IMEDIATO 
+const subscription = newObservable$.subscribe();
 
-const subscription=someObservable$.subscribe(value => console.log("sub1",value));
-setTimeout(()=>{const subscription2=someObservable$.subscribe(value=>console.log("sub2",value))},2000);
-setTimeout(()=>{subscription.unsubscribe(),console.log("cancelando a assinatura  do  primeiro")},8000)
+console.log("depois da assinatura");
