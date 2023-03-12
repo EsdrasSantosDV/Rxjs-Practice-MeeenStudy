f53ee9f8b49529ab144413a3234535c91f4df9b7
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-03-12 20:37:09 -0300

    feat:life cycle error

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Observable } from "rxjs";
+import {makeLogger} from "ts-loader/dist/logger";
 
 //O OBSERSABLE E UM GENERIC, ENTÃO ELE PODE EMITIR QUALQUER TIPO DE VALOR, COMO ELE EMITE STRINGS
 //COLOCAMOS NO GENERIC ABAIXO QU EELE RETORNA STRING
@@ -12,7 +13,9 @@ const newObservable$ = new Observable<string>((subscriber)=>{
   //AQUI ESTAMOS EMITINDO O OLIVEIRA DE FORMA ASSICRONA
 
   //O OBSERVABLE PODE EMITIR VALORES SINCRONOS E ASSICRONOS
-  setTimeout(()=>{subscriber.next("Oliveira");subscriber.complete();},2000);
+  setTimeout(()=>{subscriber.next("Oliveira")},2000);
+
+  setTimeout(()=>{subscriber.error(new Error('messagem do erro'))},6000);
 
   //LOGICA TEARDOWN E QUANDO O OBSERVABLE NOTIFICA UM COMPLETE, OU QUANDO ELE NOTIFICA UM ERRO
   return () =>{
@@ -26,7 +29,7 @@ console.log('antes da assinatura')
 
 
 
-const observer = newObservable$.subscribe({next:(value)=>console.log(value),complete:()=>console.log("finalziou"),error:()=>console.log("deu pau")
+const observer = newObservable$.subscribe({next:(value)=>console.log(value),complete:()=>console.log("finalziou"),error:(err)=>console.log(err.message)
 
 
 });
