7e268d5156ad4270f56940206160daa9f3088577
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-03-12 20:23:14 -0300

    feat:logica teardown

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Observable } from "rxjs";
 
 //O OBSERSABLE E UM GENERIC, ENTÃO ELE PODE EMITIR QUALQUER TIPO DE VALOR, COMO ELE EMITE STRINGS
 //COLOCAMOS NO GENERIC ABAIXO QU EELE RETORNA STRING
-const newObservable$ = new Observable<string>(subscriber=>{
+const newObservable$ = new Observable<string>((subscriber)=>{
   //TEMOS NOSSO SUBSCRIBER
   //E TEMOS O CICLO DE VIDA DO NOSSO OBSERVABLE
   //O NEXT E QUE ELE VAI EMITIR VARIOS VALORES, AO LONGO DO TEMPO
@@ -12,14 +12,23 @@ const newObservable$ = new Observable<string>(subscriber=>{
   //AQUI ESTAMOS EMITINDO O OLIVEIRA DE FORMA ASSICRONA
 
   //O OBSERVABLE PODE EMITIR VALORES SINCRONOS E ASSICRONOS
-  setTimeout(()=>subscriber.next("Oliveira"),2000);
-    
-  
+  setTimeout(()=>{subscriber.next("Oliveira");subscriber.complete();},2000);
+
+  //LOGICA TEARDOWN E QUANDO O OBSERVABLE NOTIFICA UM COMPLETE, OU QUANDO ELE NOTIFICA UM ERRO
+  return () =>{
+    //LOCAL PARA FORNECER O COMPORTAMENTO DE LIMPEZA E CANCELAMENTO
+    console.log("LOGICA TEARDOWN");
+  };
+  //LOGICA TEARDOWN E BOA DEMAIS, PQ AQUI PODEMOS CANCELAR PROCESSOS E ADNAMENTO QUE FORAM INCIIALIZADOS PELO OBSERVABLE
+
 });
-console.log('antes da assinatura');
+console.log('antes da assinatura')
 
 
 
-const observer = newObservable$.subscribe((value)=>console.log(value));
+const observer = newObservable$.subscribe({next:(value)=>console.log(value),complete:()=>console.log("finalziou"),error:()=>console.log("deu pau")
+
+
+});
 
 console.log("depois da assinatura");
