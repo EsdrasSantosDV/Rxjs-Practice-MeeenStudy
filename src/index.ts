e6336d29785d388bb9ec4ec9c64e71a5e45ae405
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-03-12 19:46:05 -0300

    feat:observable generic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Observable } from "rxjs";
 
-const newObservable$ = new Observable(subscriber=>{
+//O OBSERSABLE E UM GENERIC, ENTÃO ELE PODE EMITIR QUALQUER TIPO DE VALOR, COMO ELE EMITE STRINGS
+//COLOCAMOS NO GENERIC ABAIXO QU EELE RETORNA STRING
+const newObservable$ = new Observable<string>(subscriber=>{
   //TEMOS NOSSO SUBSCRIBER
   //E TEMOS O CICLO DE VIDA DO NOSSO OBSERVABLE
   //O NEXT E QUE ELE VAI EMITIR VARIOS VALORES, AO LONGO DO TEMPO
