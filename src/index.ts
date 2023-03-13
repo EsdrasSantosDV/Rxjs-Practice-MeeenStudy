420db95be852d08558f5898d25a255a2dbcea3ca
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-03-13 19:55:37 -0300

    feat:cold observable:ajax function rjxs

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,12 @@
-import { Observable } from "rxjs";
-import {makeLogger} from "ts-loader/dist/logger";
+import {ajax} from 'rxjs/ajax';
 
-//O OBSERSABLE E UM GENERIC, ENTÃO ELE PODE EMITIR QUALQUER TIPO DE VALOR, COMO ELE EMITE STRINGS
-//COLOCAMOS NO GENERIC ABAIXO QU EELE RETORNA STRING
-const newObservable$ = new Observable<number>((subscriber)=>{
-    let number=1;
-    const interval=setInterval((i:number)=>{subscriber.next(number);number++},1000);
+//AJAX FUNCTION E UMA FUNCAO DO RXJS PRA FAZER REQUISIÇOES AJAX
+const ajax$= ajax<any>('https://random-data-api.com/api/v2/users')
 
-  //LOGICA TEARDOWN E QUANDO A ASSINATURA TERMINA
-  return ()=>{
-    console.log("logica teardown");
+ajax$.subscribe(data=>console.log(data.response.first_name));
 
-    //TEMOS A LOGICA DE LIMPAR O INTERVAL , QUE SERIA COLOCAR O COMPORTAMENTO DE NÃO DEXIAR EXCESSO DE MEMORIA
-    clearInterval(interval);
-  }
-});
+ajax$.subscribe(data=>console.log(data.response.first_name));
 
+ajax$.subscribe(data=>console.log(data.response.first_name));
 
-const observer = newObservable$.subscribe({next:(value)=>console.log(value),complete:()=>console.log("finalziou"),error:(err)=>console.log(err.message)
-});
-
-const observer2 = newObservable$.subscribe({next:(value)=>console.log("2-",value),complete:()=>console.log("finalziou"),error:(err)=>console.log(err.message)
-});
-
-setTimeout(()=>observer.unsubscribe(),7000);
-setTimeout(()=>observer2.unsubscribe(),9000);
+//COLD OBSERVABLE, PRA CADA ASSINATURA TEM UMA FONTE DE EMISSÃO NOVA
